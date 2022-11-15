4afd82d3f311ed842c426799a2cb18f35cc4561b
BrunoAST <bruno-alves2010@live.com>
2022-11-14 21:51:29 -0300

    feat: create deleteTask endpoint

--- task-managment/src/tasks/tasks.controller.ts
+++ task-managment/src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { ITask } from './task.model';
 import { TasksService } from './tasks.service';
@@ -20,5 +20,10 @@ export class TasksController {
   @Get('/:id')
   getTaskById(@Param('id') id: string): ITask {
     return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
   }
 }
