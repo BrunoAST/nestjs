f82586f9270f210bc68a6f08d9f92a5cb0293bd0
BrunoAST <bruno-alves2010@live.com>
2022-11-14 21:37:35 -0300

    feat: create getTaskById endpoint

--- task-managment/src/tasks/tasks.controller.ts
+++ task-managment/src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { ITask } from './task.model';
 import { TasksService } from './tasks.service';
@@ -15,5 +15,10 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): ITask {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): ITask {
+    return this.tasksService.getTaskById(id);
   }
 }
