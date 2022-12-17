4893b983f6ddf5f04242ffb2cf5d7d4dd500c753
Bruno Alves <bruno-alves2010@live.com>
2022-12-17 16:28:15 -0300

    feat: add dto for signUp endpoint

--- bookmarks/src/auth/auth.controller.ts
+++ bookmarks/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
-  signUp() {
-    return this.authService.signIn();
+  signUp(@Body() dto: AuthDto) {
+    return this.authService.signUp();
   }
 
   @Post('sign-in')
