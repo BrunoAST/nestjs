8cb42082a19034eaeaf29f4d5ca63f548447065b
Bruno Alves <bruno-alves2010@live.com>
2022-12-18 13:09:32 -0300

    feat: add ConfigModule to read data from .env

--- bookmarks/src/prisma/prisma.service.ts
+++ bookmarks/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(readonly configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:postgres@localhost:5432/bookmarks?schema=public',
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
