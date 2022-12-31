1c0c58fe4145019380a02ea338b25173592c7e6e
Bruno Alves <bruno-alves2010@live.com>
2022-12-31 09:16:35 -0300

    fix: add missing strategy configuration

--- bookmarks/src/auth/strategy/jwt.strategy.ts
+++ bookmarks/src/auth/strategy/jwt.strategy.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super();
+  constructor(readonly configService: ConfigService) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
   }
 }
