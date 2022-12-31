23d6e05208119e7640595e26bfa498c8aa5bcdbb
Bruno Alves <bruno-alves2010@live.com>
2022-12-31 10:25:45 -0300

    feat: add validate method in jwt strategy

--- bookmarks/src/auth/strategy/jwt.strategy.ts
+++ bookmarks/src/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(readonly configService: ConfigService) {
+  constructor(
+    readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
+  ) {
     super({
       secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
   }
 }
