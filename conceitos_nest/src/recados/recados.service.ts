a8f0b5fae37bd947b2fbe1aca246e8231176aa6c
BrunoAST <bruno-alves2010@live.com>
2024-10-13 16:03:05 -0300

    feat: update recados service

--- conceitos_nest/src/recados/recados.service.ts
+++ conceitos_nest/src/recados/recados.service.ts
@@ -19,12 +19,35 @@ export class RecadosService {
   }
 
   async findAll() {
-    return await this.recadoRepository.find();
+    return await this.recadoRepository.find({
+      relations: ['de', 'para'],
+      select: {
+        de: {
+          id: true,
+          nome: true,
+        },
+        para: {
+          id: true,
+          nome: true,
+        },
+      },
+    });
   }
 
   async findOne(id: number) {
     const recado = await this.recadoRepository.findOne({
       where: { id },
+      relations: ['de', 'para'],
+      select: {
+        de: {
+          id: true,
+          nome: true,
+        },
+        para: {
+          id: true,
+          nome: true,
+        },
+      },
     });
 
     if (recado) {
@@ -35,15 +58,33 @@ export class RecadosService {
   }
 
   async create(createRecadoDto: CreateRecadoDto) {
+    const { deId, paraId } = createRecadoDto;
+
+    const de = await this.pessoaService.findOne(deId);
+
+    const para = await this.pessoaService.findOne(paraId);
+
     const novoRecado = {
-      ...createRecadoDto,
+      texto: createRecadoDto.texto,
+      de,
+      para,
       lido: false,
       data: new Date(),
     };
 
     const recado = await this.recadoRepository.create(novoRecado);
 
-    return this.recadoRepository.save(recado);
+    await this.recadoRepository.save(recado);
+
+    return {
+      ...recado,
+      de: {
+        id: recado.de.id,
+      },
+      para: {
+        id: recado.para.id,
+      },
+    };
   }
 
   async update(id: number, updateRecadoDto: UpdateRecadoDto) {
